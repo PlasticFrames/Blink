b91bb72784213a787ac2529355f708c1eca9d720
PlasticFrames <goddentr@gmail.com>
2021-06-21 13:18:36 +0100

    Replaced character controller with rigidbody

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public CharacterController controller;
+    public Rigidbody rb;
     public Transform cam;
 
     public float speed = 6f;
-
     public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    public float turnSmoothVelocity;
 
     public Vector3 moveDir;
+    public Vector3 velocity;
 
     void Start()
     {
         //Cursor.lockState = CursorLockMode.Locked; //Seems to help camera control
+        GetComponent<Rigidbody>().velocity = velocity * Time.deltaTime;
     }
 
     // Update is called once per frame
@@ -33,7 +34,13 @@ public class PlayerMove : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+
+     void FixedUpdate()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        rb.MovePosition(transform.position + input * Time.deltaTime * speed);
     }
 }
