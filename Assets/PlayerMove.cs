a1013ec7e0d6d0447876ee42c5fc5ff6eccf6223
PlasticFrames <goddentr@gmail.com>
2021-06-21 15:09:30 +0100

    Resolved merge issues

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -39,7 +39,6 @@ public class PlayerMove : MonoBehaviour
             float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
             moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
         }
     }
