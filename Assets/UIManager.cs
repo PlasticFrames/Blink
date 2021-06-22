03f3b7b7b9fa3ecf5f4250a80c078a9e101422c7
PlasticFrames <goddentr@gmail.com>
2021-06-22 17:52:46 +0100

    Brute forced a temporary UI solution to implement dash drain

---
+++ Assets/UIManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public PlayerDash dashScript;
+
+    public GameObject dash0;
+    public GameObject dash1;
+    public GameObject dash2;
+    public GameObject dash3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update() //BRUTE FORCE SOLUTION WITH LOTS OF ISSUES
+    {
+        switch(dashScript.dashNumber)
+        {
+        case 3: ;
+            dash0.SetActive (true);
+            dash1.SetActive (false);
+            dash2.SetActive (false);
+            dash3.SetActive (false);
+            break;
+        case 2: ;
+            dash0.SetActive (false);
+            dash1.SetActive (true);
+            dash2.SetActive (false);
+            dash3.SetActive (false);
+            break;
+        case 1: ;
+            dash0.SetActive (false);
+            dash1.SetActive (false);
+            dash2.SetActive (true);
+            dash3.SetActive (false);
+            break;
+        case 0: ;
+            dash0.SetActive (false);
+            dash1.SetActive (false);
+            dash2.SetActive (false);
+            dash3.SetActive (true);
+            break;
+        }    
+    }
+}
