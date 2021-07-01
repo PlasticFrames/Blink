a99fcd850fc9455bb145fedd6c55b0a0d89d12be
PlasticFrames <goddentr@gmail.com>
2021-07-01 16:54:26 +0100

    Made invulnerability more adjustable and added player flash via scale

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,16 +7,20 @@ public class PlayerHealth : MonoBehaviour
 {
     public PlayerMove moveScript;
 
+    public GameObject playerModel;
+
     public int maxHealth = 3;
     public int playerHealth = 3;
 
     public float invulnerabilityDuration = 1.5f;
+    public float invulnerabilityDelta = 0.15f;
 
     public bool isInvulnerable;
 
     void Start() 
     {
         moveScript = GameObject.FindWithTag("Player").GetComponent<PlayerMove>();
+        playerModel = GameObject.FindWithTag("Player").gameObject.transform.GetChild(2).gameObject;
     }
 
     void Update() 
@@ -46,9 +50,25 @@ public class PlayerHealth : MonoBehaviour
     {
         isInvulnerable = true;
 
-        yield return new WaitForSeconds(invulnerabilityDuration);
-
+        for (float i = 0; i < invulnerabilityDuration; i += invulnerabilityDelta)
+        {
+            if (playerModel.transform.localScale == Vector3.one)
+            {
+                ScaleModel(Vector3.zero);
+            }
+            else
+            {
+                ScaleModel(Vector3.one);
+            }
+            yield return new WaitForSeconds(invulnerabilityDelta);
+        }
+        ScaleModel(Vector3.one);
         isInvulnerable = false;
+    }
+
+    void ScaleModel(Vector3 scale) //CHANGE TO FRENEL OR SOME SUCH?
+    {
+        playerModel.transform.localScale = scale;
     }
 
     void ResetPlayer()
