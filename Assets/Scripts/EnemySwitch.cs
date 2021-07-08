de8d03b5fca5824f1d8fd9067671a41473af7261
PlasticFrames <goddentr@gmail.com>
2021-07-08 11:28:34 +0100

    Assigned dissolve materials in script

--- Assets/Scripts/EnemySwitch.cs
+++ Assets/Scripts/EnemySwitch.cs
@@ -17,11 +17,14 @@ public class EnemySwitch : MonoBehaviour
     public float heavyDissolve;
     public float mediumDissolve;
 
+    public float dissolveTime = 2f;
+
     void Start()
     {
         moveScript = GetComponent<EnemyMovement>();
         agent = GetComponent<NavMeshAgent>();
-
+        heavyDissolveMat = gameObject.transform.GetChild(4).GetComponent<Renderer>().material;
+        mediumDissolveMat = gameObject.transform.GetChild(3).GetComponent<Renderer>().material;
         CheckType();
     }
     void Update()
@@ -35,17 +38,45 @@ public class EnemySwitch : MonoBehaviour
         switch (enemyType)
         {
             case 0:
+                StartCoroutine(DissolveShield(1, dissolveTime));
                 agent.speed = moveScript.baseSpeed / (enemyType + 1);
-                Destroy(gameObject.transform.GetChild(3).gameObject);
-                Destroy(gameObject.transform.GetChild(4).gameObject);
                 break;
             case 1:
+                StartCoroutine(DissolveArmour(1, dissolveTime));
                 agent.speed = moveScript.baseSpeed / (enemyType + 1);
-                Destroy(gameObject.transform.GetChild(4).gameObject);
                 break;
             case 2:
                 agent.speed = moveScript.baseSpeed / (enemyType + 1);
                 break;
         }
+    }
+
+    IEnumerator DissolveShield(float endValue, float duration)
+    {
+        float time = 0;
+        while(time < duration)
+        {
+            heavyDissolve = Mathf.Lerp(0, 1, time / duration);
+            heavyDissolveMat.SetFloat("heavyDissolve_", heavyDissolve);
+            mediumDissolve = Mathf.Lerp(0, 1, time / duration);
+            mediumDissolveMat.SetFloat("mediumDissolve_", mediumDissolve);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(gameObject.transform.GetChild(4).gameObject);
+        Destroy(gameObject.transform.GetChild(3).gameObject);
+    }
+
+    IEnumerator DissolveArmour(float endValue, float duration)
+    {
+        float time = 0;
+        while(time < duration)
+        {
+            heavyDissolve = Mathf.Lerp(0, 1, time / duration);
+            heavyDissolveMat.SetFloat("heavyDissolve_", heavyDissolve);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(gameObject.transform.GetChild(4).gameObject);
     }
 }
